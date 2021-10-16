52eec7bfc11f5411777147c19852bf2a47980e1c
peanut <lukun1@tal.com>
2021-10-16 20:49:15 +0800

    add main

--- greeting/src/basic/mybasic.rs
+++ greeting/src/basic/mybasic.rs
@@ -5,7 +5,7 @@ struct Person<'a> {
 }
 
 enum VeryVerboseEnumOfThingsToDoWithNumbers {
-    _Subtract, // 变量不适用以_ 开头
+    Subtract, // 变量如果没有使用就以_ 开头
     Add,
 }
 // Creates a type alias
@@ -16,18 +16,91 @@ impl VeryVerboseEnumOfThingsToDoWithNumbers {
     fn run(&self, x: i32, y: i32) -> i32 {
         match self {
             Self::Add => x + y,
-            Self::_Subtract => x - y,
+            Self::Subtract => x - y,
         }
+    }
+}
+// 全局变量是在在所有其他作用域之外声明的。
+static LANGUAGE: &'static str = "Rust";
+const THRESHOLD: i32 = 10; // const 定义 不可修改
+
+fn is_big(n: i32) -> bool {
+    // 在一般函数中访问常量
+    n > THRESHOLD
+}
+
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
     }
 }
 
 fn main() {
+    // 类型转换
+    let decimal = 65.4321_f32;
+    // 可以显式转换
+    let integer = decimal as u8;
+    let character = integer as char;
+
+    println!("Casting: {} -> {} -> {}", decimal, integer, character);
+
+    let my_str = "hello";
+    let my_string = String::from(my_str);
+
+    let num = Number::from(30);
+    println!("My number is {:?},{}", num, my_string);
+
+    // 类型推断
+    // 因为有类型说明，编译器知道 `elem` 的类型是 u8。
+    let elem = 5u8;
+
+    // 创建一个空向量（vector，即不定长的，可以增长的数组）。
+    let mut vec = Vec::new();
+    // 现在编译器还不知道 `vec` 的具体类型，只知道它是某种东西构成的向量（`Vec<_>`）
+
+    // 在向量中插入 `elem`。
+    vec.push(elem);
+    // 啊哈！现在编译器知道 `vec` 是 u8 的向量了（`Vec<u8>`）。
+    // 试一试 ^ 注释掉 `vec.push(elem)` 这一行。
+    println!("{:?}", vec);
+
+    // const
+    let n = 16;
+    // 在 main 函数（主函数）中访问常量
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
+
     let name = "Peter";
     let age = 27;
     let peter = Person { name, age };
 
     let _x = Operations::Add;
 
+    let very = VeryVerboseEnumOfThingsToDoWithNumbers::run(&Operations::Subtract, 100, 20);
+
+    for n in 1..10 {
+        println!("{}", n)
+    }
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+    names.sort();
+
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            n @ "Bob" => println!("this is {}", n), // 通过@指定到变量里
+            // 处理其他情况
+            _ => println!("Hello {}", name),
+        }
+    }
+
     // 美化打印
-    println!("{:#?}", peter);
+    println!("{:#?}\nvery:{}", peter, very);
 }
--- greeting/src/biology/animal/cat.rs
+++ greeting/src/biology/animal/cat.rs
@@ -3,3 +3,139 @@ pub mod cat {
         format!("{} ", "flish")
     }
 }
+
+struct Val {
+    val: f64,
+}
+
+struct GenVal<T> {
+    gen_val: T,
+}
+
+// Val 的 `impl`
+impl Val {
+    fn value(&self) -> &f64 {
+        &self.val
+    }
+}
+
+// GenVal 的 `impl`，指定 `T` 是泛型类型
+impl<T> GenVal<T> {
+    fn value(&self) -> &T {
+        &self.gen_val
+    }
+}
+
+use std::fmt::Debug;
+
+trait PrintInOption {
+    fn print_in_option(self);
+}
+
+// 这里需要一个 `where` 从句，否则就要表达成 `T: Debug`（这样意思就变了），
+// 或着改用另一种间接的方法。
+impl<T> PrintInOption for T
+where
+    Option<T>: Debug,
+{
+    // 我们要将 `Option<T>: Debug` 作为约束，因为那是要打印的内容。
+    // 否则我们会给出错误的约束。
+    fn print_in_option(self) {
+        println!("{:?}", Some(self));
+    }
+}
+
+struct Container(i32, i32);
+
+// 这个 trait 检查给定的 2 个项是否储存于容器中
+// 并且能够获得容器的第一个或最后一个值。
+trait Contains<A, B> {
+    fn contains(&self, _: &A, _: &B) -> bool; // 显式地要求 `A` 和 `B`
+    fn first(&self) -> i32; // 未显式地要求 `A` 或 `B`
+    fn last(&self) -> i32; // 未显式地要求 `A` 或 `B`
+}
+
+impl Contains<i32, i32> for Container {
+    // 如果存储的数字和给定的相等则为真。
+    fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
+        (&self.0 == number_1) && (&self.1 == number_2)
+    }
+
+    // 得到第一个数字。
+    fn first(&self) -> i32 {
+        self.0
+    }
+
+    // 得到最后一个数字。
+    fn last(&self) -> i32 {
+        self.1
+    }
+}
+
+// 容器 `C` 就包含了 `A` 和 `B` 类型。鉴于此，必须指出 `A` 和 `B` 显得很麻烦。
+fn difference<A, B, C>(container: &C) -> i32
+where
+    C: Contains<A, B>,
+{
+    container.last() - container.first()
+}
+
+#[test]
+#[allow(unused)]
+fn test_gen() {
+    let x = Val { val: 3.0 };
+    let y = GenVal { gen_val: 100 };
+
+    println!("{}, {}", x.value(), y.value());
+
+    let vec = vec![1, 2, 3];
+    vec.print_in_option();
+
+    //关联项
+    let number_1 = 3;
+    let number_2 = 10;
+
+    let container = Container(number_1, number_2);
+
+    println!(
+        "Does container contain {} and {}: {}",
+        &number_1,
+        &number_2,
+        container.contains(&number_1, &number_2)
+    );
+    println!("First number: {}", container.first());
+    println!("Last number: {}", container.last());
+
+    println!("The difference is: {}", difference(&container));
+}
+
+use std::num::ParseIntError;
+
+// 就像 `Option` 那样，我们可以使用 `map()` 之类的组合算子。
+// 除去写法外，这个函数与上面那个完全一致，它的作用是：
+// 如果值是合法的，计算其乘积，否则返回错误。
+fn multiply(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+    first_number_str.parse::<i32>().and_then(|first_number| {
+        second_number_str
+            .parse::<i32>()
+            .map(|second_number| first_number * second_number)
+    })
+}
+
+fn print(result: Result<i32, ParseIntError>) {
+    match result {
+        Ok(n) => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+#[test]
+#[allow(unused)]
+fn test_result() {
+    // 这种情况下仍然会给出正确的答案。
+    let twenty = multiply("10", "2");
+    print(twenty);
+
+    // 这种情况下就会提供一条更有用的错误信息。
+    let tt = multiply("t", "2");
+    print(tt);
+}
